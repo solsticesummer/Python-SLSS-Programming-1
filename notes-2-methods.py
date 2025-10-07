6fd2cc2e3edce4c6c78b84e38a9767ba5e0f2fac
lostvain <solsticesummer717@gmail.com>
2025-10-07 14:54:05 -0700

    modified notes 2

--- notes-2-methods.py
+++ notes-2-methods.py
@@ -12,3 +12,11 @@ elif weather.lower().strip(".!").strip() == "sunny":
     print("You should bring sunscreen.")
 else:
     print("I see...")
+
+# Ask the customer if they want fries
+fries_reply = input("Do you want fries?")
+# "Yes!"
+if "yes" in fries_reply.lower():
+    print("Here are your fries.")
+else:
+    print("OK")
