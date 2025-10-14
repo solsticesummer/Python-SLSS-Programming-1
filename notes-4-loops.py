61fb46d6818052e23a074faec537ec51d18d0b9a
lostvain <solsticesummer717@gmail.com>
2025-10-14 14:48:52 -0700

    updated notes

--- notes-4-loops.py
+++ notes-4-loops.py
@@ -4,8 +4,20 @@
 import turtle
 
 wn = turtle.Screen()  # Set up the window and its attributes
-wn.bgcolor("cyan")
+wn.bgcolor("lightgreen")
 
 # TMNT - turtles
+mikey = turtle.Turtle()
+mikey.turtlesize(5)
+mikey.color("orange")
+mikey.pencolor("blue")
+mikey.shape("turtle")
+mikey.pendown()
+mikey.speed(1)
+mikey.left(45)
+for i in range(4):
+    mikey.forward(200)
+    mikey.right(90)
+mikey.penup()
 
 wn.exitonclick()
