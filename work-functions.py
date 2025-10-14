fb5d2d314bac8f32211dbb511fbedf2824fde87d
lostvain <solsticesummer717@gmail.com>
2025-10-14 14:21:16 -0700

    updated work

--- work-functions.py
+++ work-functions.py
@@ -1,12 +1,6 @@
-# Create a function that generates a message that changes based on a given "mood."
-# Define a function called create_mood_message.
-# It should accept two parameters: name and mood.
-# Make the mood parameter optional by giving it a default value of "neutral".
-# Inside the function, use an if/elif/else block to check the value of mood:
-# If mood is "happy", return a cheerful message like f"Hey {name}, great to see you smiling!".
-# If mood is "sad", return a supportive message like f"I hope your day gets better, {name}.".
-# If mood  is "neutral", return a message like f"Sometimes you have average days, {name}." .
-# For any other mood (the else case), return a neutral message like f"Hi {name}, hope you're having a good day.".
+# Work - Functions
+# Author - Henry
+# Ocotober 14
 
 
 def create_mood_message(mood: str = "neutral"):
