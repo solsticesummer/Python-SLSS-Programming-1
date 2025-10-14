7211a614ec9c2d10c3c0c1801c4c7f8d1544b5c9
lostvain <solsticesummer717@gmail.com>
2025-10-14 14:19:51 -0700

    added notes 4

---
+++ notes-4-loops.py
@@ -0,0 +1,11 @@
+# Drawing and Loops
+# Author: Henry Li
+# Oct 14, 2025
+import turtle
+
+wn = turtle.Screen()  # Set up the window and its attributes
+wn.bgcolor("cyan")
+
+# TMNT - turtles
+
+wn.exitonclick()
