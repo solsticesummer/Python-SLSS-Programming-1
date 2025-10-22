07319c90ca78b3b9ae994d7c0656f3b38e75000e
lostvain <solsticesummer717@gmail.com>
2025-10-22 09:10:38 -0700

    factorial

--- notes-5-recursion.py
+++ notes-5-recursion.py
@@ -3,46 +3,46 @@
 # Date: 20 Oct 2025
 
 # We're drawing trees (recursivey)
-import turtle
+# import turtle
 
-wn = turtle.Screen()
-wn.bgcolor("lightgreen")
+# wn = turtle.Screen()
+# wn.bgcolor("lightgreen")
 
-t = turtle.Turtle()
+# t = turtle.Turtle()
 
-t.left(90)
-t.color("brown")
-t.pensize(5)
-t.shape("turtle")
-t.penup()
-t.goto(0, -100)
-t.pendown()
-t.speed("fastest")
-
-
-def draw_tree(level: int, branch_length: float):
-    # Base case: if level is 0, draw a single branch
-    if level == 0:
-        t.color("blue")
-        t.stamp()
-        t.color("brown")
-        return
-
-    # Recursive case: draw a branch and two smaller trees
-    else:
-        t.forward(branch_length)
-        t.right(30)
-        draw_tree(level - 1, branch_length * 0.8)
-        t.left(60)
-        draw_tree(level - 1, branch_length * 0.8)
-        t.right(30)
-        t.backward(branch_length)
+# t.left(90)
+# t.color("brown")
+# t.pensize(5)
+# t.shape("turtle")
+# t.penup()
+# t.goto(0, -100)
+# t.pendown()
+# t.speed("fastest")
 
 
-draw_tree(5, 120)
-t.hideturtle()
+# def draw_tree(level: int, branch_length: float):
+#     # Base case: if level is 0, draw a single branch
+#     if level == 0:
+#         t.color("blue")
+#         t.stamp()
+#         t.color("brown")
+#         return
 
-wn.exitonclick()
+#     # Recursive case: draw a branch and two smaller trees
+#     else:
+#         t.forward(branch_length)
+#         t.right(30)
+#         draw_tree(level - 1, branch_length * 0.8)
+#         t.left(60)
+#         draw_tree(level - 1, branch_length * 0.8)
+#         t.right(30)
+#         t.backward(branch_length)
+
+
+# draw_tree(5, 120)
+# t.hideturtle()
+
+# wn.exitonclick()
 
 
 # import turtle
@@ -77,6 +77,22 @@ wn.exitonclick()
 #         t.right(40)
 #         t.backward(branch_length)
 
+
+def factorial(num: int) -> int:
+    if num > 1:
+        return num * factorial(num - 1)
+    else:
+        return 1
+
+
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
+print(factorial(6))
+print(factorial(7))
+print(factorial(8))
+print(factorial(9))
+print(factorial(10))
 
 # draw_complicatedtree(5, 120)
 # t.hideturtle()
