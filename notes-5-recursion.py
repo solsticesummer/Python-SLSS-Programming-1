5802e88c154e9824cd3aae69ae0b2e05f285dcea
lostvain <solsticesummer717@gmail.com>
2025-10-22 09:11:46 -0700

    d

--- notes-5-recursion.py
+++ notes-5-recursion.py
@@ -3,79 +3,79 @@
 # Date: 20 Oct 2025
 
 # We're drawing trees (recursivey)
-# import turtle
+import turtle
 
-# wn = turtle.Screen()
-# wn.bgcolor("lightgreen")
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
 
-# t = turtle.Turtle()
+t = turtle.Turtle()
 
-# t.left(90)
-# t.color("brown")
-# t.pensize(5)
-# t.shape("turtle")
-# t.penup()
-# t.goto(0, -100)
-# t.pendown()
-# t.speed("fastest")
-
-
-# def draw_tree(level: int, branch_length: float):
-#     # Base case: if level is 0, draw a single branch
-#     if level == 0:
-#         t.color("blue")
-#         t.stamp()
-#         t.color("brown")
-#         return
-
-#     # Recursive case: draw a branch and two smaller trees
-#     else:
-#         t.forward(branch_length)
-#         t.right(30)
-#         draw_tree(level - 1, branch_length * 0.8)
-#         t.left(60)
-#         draw_tree(level - 1, branch_length * 0.8)
-#         t.right(30)
-#         t.backward(branch_length)
+t.left(90)
+t.color("brown")
+t.pensize(5)
+t.shape("turtle")
+t.penup()
+t.goto(0, -100)
+t.pendown()
+t.speed("fastest")
 
 
-# draw_tree(5, 120)
-# t.hideturtle()
+def draw_tree(level: int, branch_length: float):
+    # Base case: if level is 0, draw a single branch
+    if level == 0:
+        t.color("blue")
+        t.stamp()
+        t.color("brown")
+        return
 
-# wn.exitonclick()
+    # Recursive case: draw a branch and two smaller trees
+    else:
+        t.forward(branch_length)
+        t.right(30)
+        draw_tree(level - 1, branch_length * 0.8)
+        t.left(60)
+        draw_tree(level - 1, branch_length * 0.8)
+        t.right(30)
+        t.backward(branch_length)
 
 
-# import turtle
+draw_tree(5, 120)
+t.hideturtle()
 
-# wn = turtle.Screen()
-# wn.bgcolor("skyblue")
-
-# t = turtle.Turtle()
-# t.shape("turtle")
-# t.goto(0, -100)
-# t.left(90)
-# t.speed("fastest")
-# t.pensize(5)
+wn.exitonclick()
 
 
-# def draw_complicatedtree(level: int, branch_length: float):
-#     # Base case: if level is 0, draw a single branch
-#     if level == 0:
-#         t.color("green")
-#         t.stamp()
-#         t.color("brown")
-#         return
-#     # Recursive case: draw a branch and two smaller trees
-#     else:
-#         t.forward(branch_length)
-#         t.right(40)
-#         draw_complicatedtree(level - 1, branch_length / 1.618)
-#         t.left(40)
-#         draw_complicatedtree(level - 1, branch_length / 1.618)
-#         t.left(40)
-#         draw_complicatedtree(level - 1, branch_length / 1.618)
-#         t.right(40)
-#         t.backward(branch_length)
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("skyblue")
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.goto(0, -100)
+t.left(90)
+t.speed("fastest")
+t.pensize(5)
+
+
+def draw_complicatedtree(level: int, branch_length: float):
+    # Base case: if level is 0, draw a single branch
+    if level == 0:
+        t.color("green")
+        t.stamp()
+        t.color("brown")
+        return
+    # Recursive case: draw a branch and two smaller trees
+    else:
+        t.forward(branch_length)
+        t.right(40)
+        draw_complicatedtree(level - 1, branch_length / 1.618)
+        t.left(40)
+        draw_complicatedtree(level - 1, branch_length / 1.618)
+        t.left(40)
+        draw_complicatedtree(level - 1, branch_length / 1.618)
+        t.right(40)
+        t.backward(branch_length)
 
 
 def factorial(num: int) -> int:
