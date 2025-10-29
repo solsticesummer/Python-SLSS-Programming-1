efd1cfa65d0b0a6928e21c9656e7b402d31ecba3
lostvain <solsticesummer717@gmail.com>
2025-10-29 09:49:56 -0700

    added comments

--- activity-turtle-artist-HenryLi.py
+++ activity-turtle-artist-HenryLi.py
@@ -9,7 +9,7 @@ wn = turtle.Screen()
 t = turtle.Turtle()
 
 
-# Set background
+# Set background and turtle
 wn.bgcolor("skyblue")
 t.color("white")
 t.shape("turtle")
@@ -22,8 +22,9 @@ t.goto(0, 0)
 t.left(90)
 
 
-# Draw  the bottom left window
+# Draw the top left window
 def draw_topleft_window(length: float):
+    # The recursion stops when length is less than 5
     if length < 5:
         t.penup()
         t.showturtle()
@@ -31,7 +32,7 @@ def draw_topleft_window(length: float):
         t.right(180)
 
     else:
-
+        # rightL function to draw a right L shape
         def rightL():
             t.forward(length / 2)
             t.right(90)
@@ -39,6 +40,7 @@ def draw_topleft_window(length: float):
             t.left(90)
             t.forward(length / 2)
 
+        # leftL function to draw a left L shape
         def leftL():
             t.forward(length / 2)
             t.left(90)
@@ -46,26 +48,32 @@ def draw_topleft_window(length: float):
             t.right(90)
             t.forward(length / 2)
 
+        # start drawing
         t.hideturtle()
         t.pendown()
+        # draw the outline for the new window
         for _ in range(4):
             t.forward(length)
             t.left(90)
+        # draw the cross in the new window
         leftL()
         t.right(90)
         rightL()
         t.left(90)
+        # Recursion to draw the next smaller window
         draw_topleft_window(length // 2)
 
 
+# Draw the bottom left window
 def draw_bottomleft_window(length: float):
+    # The recursion stops when length is less than 5
     if length < 5:
         t.penup()
         t.showturtle()
         t.goto(0, 0)
 
     else:
-
+        # rightL function to draw a right L shape
         def rightL():
             t.forward(length / 2)
             t.right(90)
@@ -73,6 +81,7 @@ def draw_bottomleft_window(length: float):
             t.left(90)
             t.forward(length / 2)
 
+        # leftL function to draw a left L shape
         def leftL():
             t.forward(length / 2)
             t.left(90)
@@ -80,19 +89,25 @@ def draw_bottomleft_window(length: float):
             t.right(90)
             t.forward(length / 2)
 
+        # start drawing
         t.hideturtle()
         t.pendown()
+        # draw the outline of the new window
         for _ in range(4):
             t.forward(length)
             t.right(90)
+        # draw the cross in the new window
         rightL()
         t.left(90)
         leftL()
         t.right(90)
+        # Recursion to draw the next smaller window
         draw_bottomleft_window(length // 2)
 
 
+# Draw the bottom right window
 def draw_bottomright_window(length: float):
+    # The recursion stops when length is less than 5
     if length < 5:
         t.penup()
         t.showturtle()
@@ -100,7 +115,7 @@ def draw_bottomright_window(length: float):
         t.left(180)
 
     else:
-
+        # rightL function to draw a right L shape
         def rightL():
             t.forward(length / 2)
             t.right(90)
@@ -124,16 +139,19 @@ def draw_bottomright_window(length: float):
         t.right(90)
         rightL()
         t.left(90)
+        # Recursion to draw the next smaller window
         draw_bottomright_window(length // 2)
 
 
+# Draw the topright window
 def draw_topright_window(length: float):
+    # The recursion stops when length is less than 5
     if length < 5:
         t.penup()
         t.showturtle()
 
     else:
-
+        # rightL function to draw a right L shape
         def rightL():
             t.forward(length / 2)
             t.right(90)
@@ -141,6 +159,7 @@ def draw_topright_window(length: float):
             t.left(90)
             t.forward(length / 2)
 
+        # leftL function to draw a left L shape
         def leftL():
             t.forward(length / 2)
             t.left(90)
@@ -157,13 +176,15 @@ def draw_topright_window(length: float):
         t.left(90)
         leftL()
         t.right(90)
+        # Recursion to draw the next smaller window
         draw_topright_window(length // 2)
 
 
+# run the functions
 draw_topleft_window(400)
 draw_bottomleft_window(400)
 draw_bottomright_window(400)
 draw_topright_window(400)
 
-
+# exit the window
 wn.exitonclick()
