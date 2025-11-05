9c86fbffd9cfe7cf1176d914e48d0d49f1ea93e4
lostvain <solsticesummer717@gmail.com>
2025-11-05 10:29:38 -0800

    added numbbers notes

---
+++ notes-6-numbers.py
@@ -0,0 +1,22 @@
+# Numbers and Operations
+# Author: Henry
+# Date: 2025-11-05
+
+# Work with numbers and operations
+
+# Create an algorithm to gather data to find the most popular bubble tea place around us
+
+# Version 1
+# Show all the choices
+# Ask the user for their choice
+# Keep track of a tally
+# Data analysis
+# Give raw scores
+# Give scores as a percentage
+
+# Version 2
+# Ask the user to give their favorite bubble tea place
+# Keep track of a tally
+# Data analysis
+# Give raw scores
+# Give scores as a percentage
