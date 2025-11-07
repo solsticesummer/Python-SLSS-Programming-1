08239eb04785b75470d5e1ee11cfffa5c1f4c380
lostvain <solsticesummer717@gmail.com>
2025-11-07 14:38:55 -0800

    A

--- notes-6-numbers.py
+++ notes-6-numbers.py
@@ -6,6 +6,9 @@
 
 # Create an algorithm to gather data to find the most popular bubble tea place around us
 
+import os
+
+
 # Version 1
 def vote_list_choices():
     """Display all the choices, 5 users votes for their choice, results will be printed"""
@@ -26,35 +29,58 @@ def vote_list_choices():
     heytea = 0
     coco = 0
     fresh_t = 0
+    spoiled_votes = 0
 
-    # Show all the choices
-    print("Vote for your favorite from the list")
-    print("Give the letter of your choice")
-    for choice in CHOICES:
-        print(choice)
+    for _ in range(5):
+        # Clear screen
+        os.system("clear")
 
-    # Ask the user for their choice
-    vote = input("Enter your choice: ").strip(".,?! ").lower()
+        # Show all the choices
+        print("Vote for your favorite from the list")
+        print("Give the letter of your choice")
+        for choice in CHOICES:
+            print(choice)
 
-    # Keep track of a tally
-    if vote == "a":
-        blenz += 1
-    elif vote == "b":
-        bubble_queen += 1
-    elif vote == "c":
-        sun_tea += 1
-    elif vote == "d":
-        heytea += 1
-    elif vote == "e":
-        coco += 1
-    elif vote == "f":
-        fresh_t += 1
-    else:
-        print("Invalid choice")
+        # Ask the user for their choice
+        vote = input("Enter your choice: ").strip(".,?! ").lower()
+
+        # Keep track of a tally
+        if vote == "a":
+            blenz += 1
+        elif vote == "b":
+            bubble_queen += 1
+        elif vote == "c":
+            sun_tea += 1
+        elif vote == "d":
+            heytea += 1
+        elif vote == "e":
+            coco += 1
+        elif vote == "f":
+            fresh_t += 1
+        else:
+            spoiled_votes += 1
 
     # Data analysis
     # Give raw scores
+    print("Voting results ---")
+    print(f"Blenz: {blenz} votes")
+    print(f"Bubble Queen: {bubble_queen} votes")
+    print(f"Sun Tea: {sun_tea} votes")
+    print(f"Heytea: {heytea} votes")
+    print(f"CoCo: {coco} votes")
+    print(f"Fresh T: {fresh_t} votes")
+    print(f"Spoiled Votes: {spoiled_votes} votes")
+
     # Give scores as a percentage
+    print("Vote share percentage ---")
+    total = blenz + bubble_queen + sun_tea + heytea + coco + fresh_t + spoiled_votes
+    print(f"Blenz: {blenz / total * 100} %")
+    print(f"Bubble Queen: {bubble_queen / total * 100} %")
+    print(f"Sun Tea: {sun_tea / total * 100} %")
+    print(f"Heytea: {heytea / total * 100} %")
+    print(f"CoCo: {coco / total * 100} %")
+    print(f"Fresh T: {fresh_t / total * 100} %")
+    print(f"Spoiled Votes: {spoiled_votes / total * 100} %")
 
 
 # Version 2
