4f6206a8dfae8d18c707b36a98ca97e3d7c5b6de
lostvain <solsticesummer717@gmail.com>
2025-11-12 14:42:20 -0800

    math program

--- work-maths.py
+++ work-maths.py
@@ -2,15 +2,24 @@
 # Author - Henry
 # November 12
 
+import math
+
 
 # Machines are good at crunching numbers - faster and more accurately than most humans!
 # Create a small program that calculates something useful to you (making you smile is useful).
 # It should take user input, at use at least one of the number operators we saw in class: + / * -.
 # You may modify one of your previous exercises to include calculations, if you wish.
+#
+def program():
+    print("I can help you find the square and square root of your number.")
+    num = int(input("What is your number?  ").strip(" .?,!"))
+    print(f"{math.sqrt(num)} is the square root of your number.")
+    answer = num * num
+    print(f"{answer} is the square of your number.")
 
 
 def main():
-    pass
+    program()
 
 
 if __name__ == "__main__":
