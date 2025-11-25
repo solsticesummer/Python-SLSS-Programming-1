52590077e049e90ab831fddca6650e5a6a4aefe1
lostvain <solsticesummer717@gmail.com>
2025-11-25 09:25:41 -0800

    data analysis done

--- work-dataanalysis.py
+++ work-dataanalysis.py
@@ -15,17 +15,37 @@ def main():
         file.readline()
         total = 0
         total_rainfall = 0
+        total_min_temp = 0
+        total_max_temp = 0
+        total_june = 0
         for line in file:
-            columns = line.split(",")
+            columns = line.strip("\n").split(",")
+            date = columns[0]
+            row = date.split("-")
+            month = row[1]
             precpitation = float(columns[1]) if columns[1] != "" else 0
+            min_temp = float(columns[4]) if columns[4] != "" else 0
             rainfall = precpitation
+
             total_rainfall += rainfall
             total += 1
+            total_min_temp += min_temp
+
+            if month == "06":
+                max_temp = float(columns[5]) if columns[5] != "" else 0
+                total_max_temp += max_temp
+                total_june += 1
         avg_rainfall = total_rainfall / total
+        avg_min_temp = total_min_temp / total
+        avg_max_temp = total_max_temp / total_june
+
         print(total)
         print(avg_rainfall)
+        print(avg_min_temp)
+        avg_min_temp_celsius = (avg_min_temp - 32) * 5 / 9
+        print(avg_min_temp_celsius)
+        print(avg_max_temp)
 
-    # data_points()
     avg()
 
     file.close()
