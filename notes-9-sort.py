6edbaacccbdaa83fb755004d4e58718e9f69bfc3
lostvain <solsticesummer717@gmail.com>
2025-12-04 09:31:34 -0800

    a

--- notes-9-sort.py
+++ notes-9-sort.py
@@ -54,6 +54,6 @@ def selection_sort(li: list[int], ascending=True) -> list[int]:
 
 
 if __name__ == "__main__":
-    sorted_list = selection_sort([1, 43, 55, -11, 100, 34], ascending=False)
+    sorted_list = selection_sort([1, 43, 55, -11, 100, 34], False)
 
     print(sorted_list)
