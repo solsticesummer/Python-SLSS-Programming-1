c3b9e28d7bd13aa7c4382ee89daab564cfdef364
lostvain <solsticesummer717@gmail.com>
2025-12-17 14:03:16 -0800

    pokemon class

--- notes-10-classes.py
+++ notes-10-classes.py
@@ -8,19 +8,17 @@ import random
 class Pokemon:
     def __init__(self):
         # Initialize the properties of Pokemon
-        self.name = "Pikachu"
-        self.species = "Pikachu"
-        self.type = "Electric"
-        self.shiny = False
-        self.age = 0
-        self.level = 1
-        print(f"Pokemon {self.name} is born!")
+        self.name = ""
+        self.species = ""
+        self.type = ""
         # One out of 4096 is shiny
         if random.randint(0, 4095):
             self.shiny = False
         else:
             self.shiny = True
             print(f"{self.name} is shiny!")
+
+        print("A Pokemon is born!")
 
     def talk(self):
         """Hear what the pokemon has to say
@@ -33,8 +31,6 @@ class Pokemon:
         print(f"    Species: {self.species}")
         print(f"    Type: {self.type}")
         print(f"    Shiny: {self.shiny}")
-        print(f"    Age: {self.age}")
-        print(f"    Level: {self.level}")
         print("--------------------")
 
     def dance(self):
@@ -46,36 +42,57 @@ class Pokemon:
 
 
 class squirtle(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.name = "Squirtle"
+        self.species = "Squirt"
+        self.type = "Water"
+
     def water_gun(self):
         """Shoots a water gun"""
         print(f"{self.name} shoots a water gun!")
         # TODO: check to see if its effective
 
 
+class charizard(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.name = "Charizard"
+        self.species = "Char"
+        self.type = "Fire"
+
+    def fireball(self):
+        print(f"{self.name} shoots a fireball!")
+
+
 if __name__ == "__main__":
     # Create a pokemon object
     pokemon1 = Pokemon()
+    pokemon1.name = "Pikachu"
     # View its properties
     print("Pokemon Name:", pokemon1.name)
     # Change some properties
-    pokemon1.name = "Charizard"
-    print("Pokemon Name:", pokemon1.name)
+    pokemon1.shiny = True
     # Create another pokemon object
     pokemon2 = Pokemon()
+    pokemon2.name = "Bulbasaur"
+    pokemon2.shiny = False
     print("Pokemon Name:", pokemon2.name)
     # Compare the two objects
     if pokemon1 == pokemon2:
         print("The two pokemons are the same.")
     else:
         print("The two pokemons are different.")
-    pokemon1.talk()
-    pokemon2.talk()
-    pokemon1.stats()
-    pokemon2.stats()
-    pokemon1.dance()
-    pokemon2.dance()
 
+    # Squirtle
     squirtle1 = squirtle()
-    squirtle1.name = "Squirtle"
+    squirtle1.stats()
     squirtle1.talk()
     squirtle1.water_gun()
+
+    # Charizard
+    charizard1 = charizard()
+    charizard1.name = "Charizard"
+    charizard1.stats()
+    charizard1.talk()
+    charizard1.fireball()
