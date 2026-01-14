7dc3b14ffeef7fd090fe6b02d600c1c48fc23f0a
lostvain <solsticesummer717@gmail.com>
2026-01-14 14:40:20 -0800

    pygame

--- Pygame Final/pygamefinal-Henry-Li.py
+++ Pygame Final/pygamefinal-Henry-Li.py
@@ -2,7 +2,28 @@
 # Author: Henry
 # 14 January 2026
 
+import random
+
 import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 100
+        self.rect.centery = 100
+
+
+class Lazer(pygame.sprite.Sprite):
+    def __init(self):
+        super().__init__()
+
+        self.image = pygame.image.load("assets/lazer.png")
+        self.rect = self.image.get_rect()
+
 
 def game():
     pygame.init()
@@ -10,24 +31,26 @@ def game():
     # COLOURS - (R, G, B)
     # CONSTANTS ALL HAVE CAPS FOR THEIR NAMES
     WHITE = (255, 255, 255)
-    BLACK = (  0,   0,   0)
-    RED   = (255,   0,   0)
-    GREEN = (  0, 255,   0)
-    BLUE  = (  0,   0, 255)
-    GREY  = (128, 128, 128)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    GREY = (128, 128, 128)
 
     # CONSTANTS
     WIDTH = 800
     HEIGHT = 600
     SIZE = (WIDTH, HEIGHT)
+    TITLE = "LAZER"
 
     # Creating the Screen
     screen = pygame.display.set_mode(SIZE)
-    pygame.display.set_caption("Your Title Here")
+    pygame.display.set_caption(TITLE)
 
     # Variables
     done = False
     clock = pygame.time.Clock()
+    score = 0
 
     # ------------ MAIN GAME LOOP
     while not done:
@@ -38,19 +61,19 @@ def game():
                 done = True
 
         # ------ GAME LOGIC
-
+        lazer_collided = pygame.sprite.Spritecollide(Player, Lazer, True)
+        if lazer_collided:
+            print(f"Score: {player.calc_damage}")
         # ------ DRAWING TO SCREEN
-        screen.fill(WHITE)
-        # draw a red rectangle in the middle of the screen
-        pygame.draw.rect(screen, RED, (WIDTH / 2, HEIGHT / 2, 100, 40))
-
+        screen.fill(BLACK)
         # Update screen
         pygame.display.flip()
 
         # ------ CLOCK TICK
-        clock.tick(60) # 60 fps
+        clock.tick(60)  # 60 fps
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     game()
